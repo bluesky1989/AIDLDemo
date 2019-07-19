ab04dec9c46a2f31d1b82c5ad9e2eb4978f218e8
yorkdu <duyongqiang1989@gmail.com>
2019-07-19 13:17:41 +0800

    Update MainActivity.java
    测试github桌面版

--- app/src/main/java/com/dyq/aidldemo/MainActivity.java
+++ app/src/main/java/com/dyq/aidldemo/MainActivity.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+	//测试
     private static final String TAG="MainActivity";
 
     private ServiceConnection serviceConnection=new ServiceConnection() {
